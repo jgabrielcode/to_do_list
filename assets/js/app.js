4f7814d96abba76216907ec3c77247e5b72d2e16
João Gabriel De Freitas Costa <joao1306gabriel@gmail.com>
2025-07-13 20:49:30 -0300

    Salvando dados no local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
-const listContainer = document.querySelector("#list-container")
 const inBox = document.querySelector("#inBox")
 const btEnviar = document.querySelector("#btEnviar")
+
+const listContainer = document.querySelector("#list-container")
+
 btEnviar.addEventListener("click",()=>{
     
     const tarefas = inBox.value
@@ -11,20 +13,24 @@ btEnviar.addEventListener("click",()=>{
     criaLinhas(tarefas)
     inBox.value = ""
     inBox.focus()
+    saveData()
 
 })
 
-document.addEventListener("click",(e)=>{
+listContainer.addEventListener("click",(e)=>{
     const el = e.target
     if(el.tagName == "LI"){
         el.classList.toggle("checked")
+        saveData()
     }
      if(el.tagName == "I"){
          removerItem(el)
+         saveData()
      }
 })
 
 function criaLinhas(tarefas){
+
     const li = document.createElement('li')
     const i = document.createElement('i')
     i.className = "bi bi-trash3-fill"
@@ -39,3 +45,13 @@ function criaLinhas(tarefas){
 function removerItem(e){
     e.parentElement.remove()
 }
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+function loadData(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+loadData()
